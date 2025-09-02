b7c3d1d570bef6aae2ec0d9a434510e2069e4b24
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-09-02 11:51:29 +0530

    publish file changes

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -51,12 +51,14 @@ class AdminServiceProvider extends ServiceProvider
             
         $slug = $admin->website_slug ?? 'admin';
 
+        $routeFile = base_path('Modules/Admins/routes/web.php');
+        if (!file_exists($routeFile)) {
+            $routeFile = __DIR__ . '/routes/web.php'; // fallback to package route
+        }
 
         Route::middleware('web')
             ->prefix("{$slug}/admin") // dynamic prefix
-            ->group(function () {
-                $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            });
+            ->group($routeFile);
     }
 
     public function register()
